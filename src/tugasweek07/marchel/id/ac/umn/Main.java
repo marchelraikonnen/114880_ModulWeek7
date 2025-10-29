5eb0d2324188ca340bbb5fd6093cb9ac87fa866b
marchelraikonnen <marchelraikonnen19@gmail.com>
2025-10-29 10:32:53 +0700

    Item class

---
+++ src/tugasweek07/marchel/id/ac/umn/Main.java
@@ -0,0 +1,14 @@
+package tugasweek07.marchel.id.ac.umn;
+
+public class Main {
+    public static void main(String[] args) {
+        Payment creditCardPayment = new CreditCardPayment(100.0, "1234-5678-9012-3456");
+        Payment bankTransferPayment = new BankTransferPayment(300.0, "9876543210");
+
+        creditCardPayment.paymentDetails();
+        creditCardPayment.processPayment();
+
+        bankTransferPayment.paymentDetails();
+        bankTransferPayment.processPayment();
+    }
+}
