b1f24d6f6d5b1abf20b3c40d0cca9a741f497ad6
marchelraikonnen <marchelraikonnen19@gmail.com>
2025-10-29 10:16:08 +0700

    Abstract class payment

---
+++ src/tugasweek07/marchel/id/ac/umn/Payment.java
@@ -0,0 +1,15 @@
+package tugasweek07.marchel.id.ac.umn;
+
+public abstract class Payment {
+    protected double amount;
+
+    public Payment(double amount) {
+        this.amount = amount;
+    }
+
+    abstract void processPayment();
+
+    public void paymentDetails() {
+        System.out.println("Processing payment of $" + amount);
+    }
+}
